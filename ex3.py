9eee55741f1c9c33fa9c9c216050e0a81501c018
CumFlakes <a190082@lazarocardenas.edu.mx>
2021-10-25 19:40:10 -0700

    octavo commit

--- ex3.py
+++ ex3.py
@@ -1,21 +1,26 @@
 print ("I will now count my chickens:")
 
-print ("Hens", 25 + 30 / 6)
-print ("Roosters", 100 - 25 * 3 % 4)
+#En esta linea cuenta las gallinas
+print ("Hens", 25 + 30 / 6.0)
+#En esta los gallos
+print ("Roosters", 100 - 25 * 3 % 4.0)
 
 print ("Now I will count the eggs:")
-
-print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#Cuenta los huevos
+print (3 + 2 + 1 - 5 + 4 % 2.0 - 1 / 4 + 6)
 
 print ("Is it true that 3 + 2 < 5 - 7?")
-
+#Es la operacion para comprobar si 3 + 2 < 5 - 7
 print (3 + 2 < 5 - 7)
-
+#Se suma 3+2
 print ("What is 3 + 2?", 3 + 2)
+#Se resta 5-7
 print ("What is 5 - 7?", 5 - 7)
 
 print ("How about some more.")
-
+#Pregunta si 3 es mayor que menos 2
 print ("Is it greater?", 3 > -2)
+#Pregunta si 5 es mayor o igual que menos 2
 print ("Is it greater or equal?", 5 >= -2)
+#Pregunta si 5 en menor o igual que menos 2
 print ("Is it less or equal?", 5 <= -2) 
